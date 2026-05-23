11ff6e3cd7264f6a3fd5772bf33635552bbce521
Federico M. Iachetti <iachetti.federico@gmail.com>
2026-05-23 19:49:10 -0300

    Pass pkgs-master to myMachine nixosSystem

--- modules/hosts/my-machine/default.nix
+++ modules/hosts/my-machine/default.nix
@@ -1,5 +1,9 @@
 { self, inputs, ... }: {
   flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {
+      pkgs-master = import inputs.nixpkgs-master { system = "x86_64-linux"; };
+    };
+
     modules = [
       self.nixosModules.myMachineConfiguration
     ];
