176f8929f71b5062dfbe4e380f52465a6368b9d3
Federico M. Iachetti <iachetti.federico@gmail.com>
2026-05-24 10:42:44 -0300

    Extract pipewire feature module

--- modules/hosts/toph/configuration.nix
+++ modules/hosts/toph/configuration.nix
@@ -7,6 +7,7 @@
       self.nixosModules.cli
       self.nixosModules.fonts
       self.nixosModules.openssh
+      self.nixosModules.pipewire
       self.nixosModules.i3
       self.nixosModules.opencode
     ];
@@ -73,21 +74,6 @@
     # Enable CUPS to print documents.
     services.printing.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
 
     # Enable touchpad support (enabled default in most desktopManager).
     # services.xserver.libinput.enable = true;
