fe7f1c0df86831afb897fef22a0c7298f9b30958
Federico M. Iachetti <iachetti.federico@gmail.com>
2026-08-15 11:40:35 -0300

    Set Sofi locale to Spanish

--- modules/users/sofi/default.nix
+++ modules/users/sofi/default.nix
@@ -7,5 +7,12 @@
       shell = pkgs.zsh;
       packages = with pkgs; [ ];
     };
+
+    environment.loginShellInit = ''
+      if [ "$USER" = "sofi" ]; then
+        export LANG="es_AR.UTF-8"
+        export LANGUAGE="es_AR:es"
+      fi
+    '';
   };
 }
